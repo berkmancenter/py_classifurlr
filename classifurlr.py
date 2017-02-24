57a3ee24cec6934770afb4cea60dca10072ed04a
jdcc <jclark@cyber.law.harvard.edu>
2017-02-24 11:55:19 -0600

    use dice coefficient

--- classifurlr.py
+++ classifurlr.py
@@ -370,12 +370,49 @@ class DifferingDomainClassifier(Classifier):
                 'are significantly different')
         self.pad_domain_to = 50
         self.multiplier = 2.5 # Ratios of very different URLs were around 0.28
+        self.use_dice = True
+
+    # https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Dice%27s_coefficient#Python
+    def dice_coefficient(self, a, b):
+        if not len(a) or not len(b): return 0.0
+        """ quick case for true duplicates """
+        if a == b: return 1.0
+        """ if a != b, and a or b are single chars, then they can't possibly match """
+        if len(a) == 1 or len(b) == 1: return 0.0
+
+        """ use python list comprehension, preferred over list.append() """
+        a_bigram_list = [a[i:i+2] for i in range(len(a)-1)]
+        b_bigram_list = [b[i:i+2] for i in range(len(b)-1)]
+
+        a_bigram_list.sort()
+        b_bigram_list.sort()
+
+        # assignments to save function calls
+        lena = len(a_bigram_list)
+        lenb = len(b_bigram_list)
+        # initialize match counters
+        matches = i = j = 0
+        while (i < lena and j < lenb):
+            if a_bigram_list[i] == b_bigram_list[j]:
+                matches += 2
+                i += 1
+                j += 1
+            elif a_bigram_list[i] < b_bigram_list[j]:
+                i += 1
+            else:
+                j += 1
+
+        score = float(matches)/float(lena + lenb)
+        return score
 
     def get_diff_ratio(self, a, b):
         # return 1 if they are very different, 0 if identical
-        ratio = difflib.SequenceMatcher(None,
-                a.zfill(self.pad_domain_to), b.zfill(self.pad_domain_to)).ratio()
-        return min(1.0, (1 - ratio) * self.multiplier)
+        if self.use_dice:
+            return 1.0 - self.dice_coefficient(a, b)
+        else:
+            ratio = difflib.SequenceMatcher(None,
+                    a.zfill(self.pad_domain_to), b.zfill(self.pad_domain_to)).ratio()
+            return min(1.0, (1 - ratio) * self.multiplier)
 
     def extract_domain(self, url):
         return tldextract.extract(url).registered_domain
